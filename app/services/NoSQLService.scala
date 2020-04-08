a334ebac36e49e90f2e7483ced4569216189b2ac
Bimal Bhagrath <bimal@carbonate.app>
2020-04-08 18:27:36 -0400

    Create NoSQL service for MongoDB

---
+++ app/services/NoSQLService.scala
@@ -0,0 +1,25 @@
+package io.bimo2.stackshare
+
+import scala.concurrent.Await
+
+import org.mongodb.scala._
+import scala.concurrent.duration._
+
+object NoSQLService {
+
+  private val client = MongoClient()
+  private val database = client.getDatabase("stackshare")
+  private val timeout = 10.seconds
+
+  def destroy(): Unit = {
+    Await.result(users().drop().toFuture(), timeout)
+    Await.result(positions().drop().toFuture(), timeout)
+
+    database.createCollection("users")
+    database.createCollection("positions")
+  }
+
+  private def users(): MongoCollection[Document] = database.getCollection("users")
+
+  private def positions(): MongoCollection[Document] = database.getCollection("positions")
+}
--- build.sbt
+++ build.sbt
@@ -6,3 +6,4 @@ scalaVersion := "2.13.1"
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 libraryDependencies += "com.typesafe.play" %% "play" % "2.8.1"
+libraryDependencies += "org.mongodb.scala" %% "mongo-scala-driver" % "2.9.0"
