1bcd2fbe8f174f169b117acd7a6a45e4e7c3c4e7
Bimal Bhagrath <bimal@carbonate.app>
2020-04-11 13:39:46 -0400

    Create user client script

---
+++ public/main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const path = 'http://localhost:9000';
+const content = {
+  'Content-Type': 'application/json'
+};
+
+const isOk = (response) => {
+  if (response.ok) return response;
+
+  window.location.href = `${path}/error/${response.status}`;
+};
+
+const onCreateUser = async () => {
+  const attributeNodes = document.getElementsByName('attributes');
+  const attributes = Array.from(attributeNodes);
+
+  const data = {
+    username: document.getElementsByName('username')[0].value || undefined,
+    attributes: Object.assign(...attributes.map((attribute) => {
+      const score = parseInt(attribute.value);
+
+      return !!score ? {
+        [attribute.dataset.lang]: score
+      } : {};
+    }))
+  };
+
+  if (data.username && !!Object.keys(data.attributes).length) {
+    let response = await fetch(`${path}/users`, {
+      method: 'POST',
+      headers: { ...content },
+      body: JSON.stringify(data)
+    });
+
+    isOk(response);
+
+    let json = await response.json();
+
+    window.location.href = `${path}/users/${json.username}`;
+  }
+
+  return false;
+};
